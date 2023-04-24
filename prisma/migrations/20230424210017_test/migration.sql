c171d357d49f023d18aaced654ea51235b8862fb
Vinicius <vinicius@pop-os.localdomain>
2023-04-24 18:49:26 -0300

    chore: docker config

---
+++ prisma/migrations/20230424210017_test/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
+);
